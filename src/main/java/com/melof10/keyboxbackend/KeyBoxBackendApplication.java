c6333c8033a0c4be8a8057035334dee892e6ec4d
melof10 <95fedemelo@gmail.com>
2023-05-28 23:30:21 -0300

    first commit

---
+++ src/main/java/com/melof10/keyboxbackend/KeyBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.melof10.keyboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyBoxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyBoxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melof10/keyboxbackend/KeyBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melof10.keyboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyBoxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
